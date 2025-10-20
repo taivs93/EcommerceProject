5b7058db6aa76f86c672eddec29dccf77d82a18e
taivs93 <taivs0903@gmail.com>
2025-10-20 22:42:39 +0700

    Init

---
+++ src/app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const { default: helmet} = require('helmet');
+const compression = require('compression');
+
+//init middlewares
+
+//morgan để đọc logs.
+app.use(morgan("dev")); //Phù hợp với môi trường dev.
+// app.use(morgan("combined")) //Phù hợp với product.
+// app.use(morgan("common"))
+// app.use(morgan("short"))
+// app.use(morgan("tiny"))
+
+//helmet để bảo vệ header cho ứng dụng express.
+app.use(helmet()); 
+//compress dùng để nén dữ liệu trả về cho client.
+app.use(compression());
+
+//init db
+
+
+//init routes
+app.get('/', (req, res, next) => {
+    return res.status(200).json({
+        message: "Welcome"
+    });
+});
+
+//handling error
+
+module.exports = app;
+
